d98dc2c289a9920f056aab0b719c7013503140f7
Richa Singhal <singhalricha40@gmail.com>
2026-06-26 00:31:43 +0530

    chore: remove environment files from tracking, add example

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  supabaseUrl: 'https://iquyikwdtfozwikfuljr.supabase.co',
-  supabaseKey: 'sb_publishable_iYtbjZJNgDp2k5fmJH04Uw_YZ9dk8a7' // same key
-};
